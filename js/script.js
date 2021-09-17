261ec06be3e452e92b060429958053abd6ddb687
Harmew <dann.ew@outlook.com>
2021-09-17 16:32:59 -0300

    navegacao prev e next finalizada

--- js/script.js
+++ js/script.js
@@ -1,8 +1,5 @@
-import Slide from "./slide.js";
+import { SlideNav } from "./slide.js";
 
-const slide = new Slide(".slide", ".slide-wrapper");
+const slide = new SlideNav(".slide", ".slide-wrapper");
 slide.init();
-
-
-slide.changeSlide(3)
-slide.activePrevSlide()
+slide.addArrow('.prev', '.next')
